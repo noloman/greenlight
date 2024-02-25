5183d92dc0e78b905b72138aac593115674599da
Manuel Lorenzo <manulorenzop@gmail.com>
2024-02-25 21:59:49 +0100

    👽️ successfully GET a movie from the DB

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,12 +1,11 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"net/http"
-	"time"
-
 	"github.com/noloman/greenlight/internal/data"
 	"github.com/noloman/greenlight/internal/data/validator"
+	"net/http"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -55,13 +54,15 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		app.notFoundResponse(w, r)
 	}
 
-	movie := data.Movie{
-		ID:        id,
-		CreatedAt: time.Now(),
-		Title:     "Some movie",
-		Runtime:   120,
-		Genres:    []string{"drama", "comedy", "action"},
-		Version:   1,
+	movie, err := app.models.Movies.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
 	}
 
 	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
--- internal/data/movies.go
+++ internal/data/movies.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"database/sql"
+	"errors"
 	"time"
 
 	"github.com/lib/pq"
@@ -29,7 +30,30 @@ func (m MovieModel) Insert(movie *Movie) error {
 }
 
 func (m MovieModel) Get(id int64) (*Movie, error) {
-	return nil, nil
+	if id < 1 {
+		return nil, ErrRecordNotFound
+	}
+	query := `SELECT id, created_at, title, year, runtime, genres, version FROM movies WHERE id = $1`
+	movie := &Movie{}
+	err := m.DB.QueryRow(query, id).
+		Scan(&movie.ID,
+			&movie.CreatedAt,
+			&movie.Title,
+			&movie.Year,
+			&movie.Runtime,
+			pq.Array(&movie.Genres),
+			&movie.Version,
+		)
+
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
+	return movie, nil
 }
 
 func (m MovieModel) Update(movie *Movie) error {
